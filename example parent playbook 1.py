644c7ddfe95a869466c873566e0dd12b40246696
soar_local_admin <root@localhost>
2024-06-13 23:10:50 +0000

    testing for soarhelp ticket

---
+++ example parent playbook 1.py
@@ -0,0 +1,54 @@
+"""
+
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+@phantom.playbook_block()
+def on_start(container):
+    phantom.debug('on_start() called')
+
+    # call 'playbook_example_child_playbook_1' block
+    playbook_example_child_playbook_1(container=container)
+
+    return
+
+@phantom.playbook_block()
+def playbook_example_child_playbook_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("playbook_example_child_playbook_1() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    # call playbook "local/example child playbook", returns the playbook_run_id
+    playbook_run_id = phantom.playbook("local/example child playbook", container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
